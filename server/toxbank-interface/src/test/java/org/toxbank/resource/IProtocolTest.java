08687967b67de34fea2ff7d2d64dc2461776bc70
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-11-04 14:54:07 +0200

    A Test

---
+++ server/toxbank-interface/src/test/java/org/toxbank/resource/IProtocolTest.java
@@ -0,0 +1,13 @@
+package org.toxbank.resource;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class IProtocolTest {
+
+	@Test
+	public void test() {
+		Assert.assertTrue("Fake test to test tests :)",true);
+	}
+}
