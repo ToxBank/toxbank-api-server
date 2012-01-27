cce8122fc8c24406ba33bdfb2ac277fbbfb916c4
ngn <jeliazkova.nina@gmail.com>
2012-01-27 21:38:53 +0200

    Looks like RDF serialization for tasks was missing - not anymore

--- toxbank-war/src/main/java/org/toxbank/demo/task/TBTaskResource.java
+++ toxbank-war/src/main/java/org/toxbank/demo/task/TBTaskResource.java
@@ -1,8 +1,21 @@
 package org.toxbank.demo.task;
 
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.UUID;
+
+import net.idea.modbcum.i.exceptions.AmbitException;
+import net.idea.modbcum.i.reporter.Reporter;
+import net.idea.restnet.c.ResourceDoc;
 import net.idea.restnet.c.html.HTMLBeauty;
 import net.idea.restnet.c.resource.TaskResource;
+import net.idea.restnet.c.task.FactoryTaskConvertor;
+import net.idea.restnet.i.task.ITaskStorage;
+import net.idea.restnet.rdf.reporter.TaskRDFReporter;
 
+import org.restlet.Request;
+import org.restlet.representation.Variant;
+import org.restlet.resource.ResourceException;
 import org.toxbank.rest.protocol.TBHTMLBeauty;
 
 public class TBTaskResource extends TaskResource<String> {
@@ -10,5 +23,18 @@ public class TBTaskResource extends TaskResource<String> {
 	@Override
 	protected HTMLBeauty getHTMLBeauty() {
 		return new TBHTMLBeauty();
+	}
+	
+	@Override
+	protected FactoryTaskConvertor getFactoryTaskConvertor(ITaskStorage storage)
+			throws ResourceException {
+		return new FactoryTaskConvertor<Object>(storage,getHTMLBeauty()) {
+			@Override
+			public synchronized Reporter<Iterator<UUID>, Writer> createTaskReporterRDF(
+					Variant variant, Request request, ResourceDoc doc)
+					throws AmbitException, ResourceException {
+				return new TaskRDFReporter(storage,request,variant.getMediaType(),doc);
+			}
+		};
 	}
 }
