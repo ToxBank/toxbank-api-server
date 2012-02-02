9633d307a4c3848243a9f541f2c9c52aaf613286
ngn <jeliazkova.nina@gmail.com>
2012-02-02 13:09:43 +0200

    Allows protocol file update

--- toxbank-protocol/src/main/java/org/toxbank/rest/protocol/db/UpdateProtocol.java
+++ toxbank-protocol/src/main/java/org/toxbank/rest/protocol/db/UpdateProtocol.java
@@ -43,6 +43,7 @@ public class UpdateProtocol extends AbstractObjectUpdate<DBProtocol>{
 			ReadProtocol.fields.title,
 			ReadProtocol.fields.anabstract,
 			ReadProtocol.fields.summarySearchable,
+			ReadProtocol.fields.filename,
 			ReadProtocol.fields.idproject,
 			ReadProtocol.fields.idorganisation,
 			ReadProtocol.fields.iduser,
