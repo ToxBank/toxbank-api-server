ad0624005b1009ac63d2a5cdc7f2dd5bf3dc3393
ngn <jeliazkova.nina@gmail.com>
2014-04-16 11:16:35 +0300

    DB version changed

--- toxbank-protocol/src/main/java/org/toxbank/rest/protocol/db/DbCreateDatabase.java
+++ toxbank-protocol/src/main/java/org/toxbank/rest/protocol/db/DbCreateDatabase.java
@@ -52,7 +52,7 @@ import net.idea.modbcum.p.AbstractDBProcessor;
  */
 public class DbCreateDatabase extends AbstractDBProcessor<String,String> {
 	
-	public final static String version = "1.10";
+	public final static String version = "1.11";
 	
     /**
 	 */
