58cec9c23b91aa26a6846d85bf320910ae346cb8
ngn <jeliazkova.nina@gmail.com>
2014-07-02 14:15:26 +0300

    search for everything

--- toxbank-protocol/src/main/resources/org/toxbank/rest/protocol/db/sql/monthly_alerts.sql
+++ toxbank-protocol/src/main/resources/org/toxbank/rest/protocol/db/sql/monthly_alerts.sql
@@ -1,11 +1,12 @@
 insert into alert
 SELECT null,
 "ToxBank Monthly alert",
-"?resourceType=protocol&resourceType=investigation",
+"?metadata=*&resourceType=protocol&resourceType=investigation",
 "FREETEXT",
 "monthly",
 1,
 iduser,
 now(),
 concat(year(now())-1,'-',month(now()),'-1 0:0:0.0')
- FROM user;
+ FROM user
+ ;
